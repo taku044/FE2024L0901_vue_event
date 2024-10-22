203748161b1d3c1934dbb29d070cea07c7aebe60
taku044 <takuitiani0416606@gmail.com>
2024-10-22 09:55:45 +0900

    カウンターアプリ作成

--- 3-2/script/script.js
+++ 3-2/script/script.js
@@ -1,1 +1,18 @@
 // JavaScript用ファイル
+// JavaScript用ファイル
+new Vue({
+    el: '#app',
+    data() {
+        return{
+           count:0
+        };
+    },
+    methods: {
+        increment(){
+            this.count++;
+        },
+        decrement(){
+            this.count--;
+        }
+    }
+});
